075296718ff0382db2548e07d90de44e534469ec
Viraj Patidar <vrj381@gmail.com>
2021-11-11 15:55:45 +0530

    delete note completed

--- controllers/noteController.go
+++ controllers/noteController.go
@@ -74,3 +74,37 @@ func GetNotes(c *fiber.Ctx) error {
 	return c.JSON(notes)
 }
 
+func DeleteNote(c *fiber.Ctx) error {
+	cookie := c.Cookies("jwt")
+
+	_, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(SecretKey), nil
+	})
+
+	if err != nil {
+		c.Status(fiber.StatusUnauthorized)
+		return c.JSON(fiber.Map{
+			"message": "unauthenticated",
+		})
+	}
+
+
+	note_id := c.Params("NoteID")
+	var note models.Note
+
+	database.DB.Where("id = ?", note_id).Find(&note)
+
+	if note.ID == 0 {
+		c.Status(fiber.StatusNotFound)
+		return c.JSON(fiber.Map{
+			"message": "Note not found",
+		})
+	}
+
+	//database.DB.Unscoped().Delete(&note) // Permanent deletion
+	database.DB.Delete(&note) // Soft deletion 
+
+	return c.JSON(fiber.Map{
+		"message": "Note deleted successfully",
+	})
+}
