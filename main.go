5c02e467a8eaebb1ff2f1876a1913adb45fff2da
Viraj Patidar <vrj381@gmail.com>
2021-12-24 14:09:49 +0530

    CORS

--- main.go
+++ main.go
@@ -17,6 +17,8 @@ func main() {
 	port := os.Getenv("PORT")
 
 	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:3000/",
+		AllowHeaders:  "Origin, Content-Type, Accept",
 		AllowCredentials: true,
 	}))
 
